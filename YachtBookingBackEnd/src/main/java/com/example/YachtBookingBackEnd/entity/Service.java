6a4f566d84d074222fed9875d3dd88a2810be37c
Tran Quoc Viet <viettqhe170804@fpt.edu.vn>
2024-06-12 00:27:25 +0700

    Modify

--- YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/entity/Service.java
+++ YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/entity/Service.java
@@ -1,11 +1,9 @@
 package com.example.YachtBookingBackEnd.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.util.Set;
 
 @Getter
--- YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/service/implement/ICustomer.java
+++ YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/service/implement/ICustomer.java
@@ -1,6 +1,5 @@
 package com.example.YachtBookingBackEnd.service.implement;
 
-import com.example.YachtBookingBackEnd.entity.Customer;
 
 public interface ICustomer {
     boolean addCustomer(String idAccount, String fullName, String email, String phoneNumber, String address);
