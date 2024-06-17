a1e660f09fbab9f9916db023501397918070fa6d
dinh vuong <dinhvuong2311@gmail.com>
2024-06-17 16:09:31 +0700

    issue yacht image dto

--- YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/service/implement/IYachtImage.java
+++ YachtBookingBackEnd/src/main/java/com/example/YachtBookingBackEnd/service/implement/IYachtImage.java
@@ -2,12 +2,11 @@ package com.example.YachtBookingBackEnd.service.implement;
 
 import com.example.YachtBookingBackEnd.dto.YachtImageDTO;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 
 public interface IYachtImage {
     List<YachtImageDTO> getImageByYacht(String yachtId);
     boolean addImage(MultipartFile image, String yachtId);
     boolean deleteImage(String imageId);
-    public boolean updateImage(MultipartFile image, String imageId);
+    boolean updateImage(MultipartFile image, String imageId);
 }
