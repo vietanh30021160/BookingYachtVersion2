683f2761694d608cb2fe64cd559e9b177a73707c
Phạm Thanh Tuấn <pham34074@gmail.com>
2024-06-21 22:03:16 +0700

    fix-frontend

--- front_end/bookingyacht/src/components/enterprise/ViewInfoCompany.js
+++ front_end/bookingyacht/src/components/enterprise/ViewInfoCompany.js
@@ -1,55 +1,69 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
-import { Card, Col, Container, Row, Spinner } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './Enterprise.scss';
 
 const CompanyList = () => {
     const [companies, setCompanies] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
     const getImageApi = `http://localhost:8080/api/customer/file/`
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/customer/getAllCompany')
             .then(res => {
                 setCompanies(res.data.data.filter(c => c.exist === 1));
-                setLoading(false);
+                // setLoading(false);
             })
-
             .catch(error => {
-                setLoading(false);
-            })
+                // setLoading(false);
+                
+            });
     }, []);
 
-    if (loading) {
-        return <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-        </Spinner>
-    }
+    // if (loading) {
+    //     return <Spinner animation="border" role="status">
+    //         <span className="sr-only">Loading...</span>
+    //     </Spinner>
+    // }
+
+    // Chia mảng companies thành các mảng con với kích thước mỗi mảng con là 2.
+    const chunkArray = (array, chunkSize) => {
+        const chunks = [];
+        for (let i = 0; i < array.length; i += chunkSize) {
+            chunks.push(array.slice(i, i + chunkSize));
+        }
+        return chunks;
+    };
+
+    const companyChunks = chunkArray(companies, 2);
 
     return (
         <Container>
-            <Row>
-                {
-                    companies.map(company => (
-                        <Col key={company.idCompany} xs={12} md={6} lg={4}>
-                            {/* <CompanyCard company={company} getImageApi={getImageApi}/> */}
-                            <Card style={{ width: '18rem', margin: '10px' }}>
-                                {/* <Card.Img variant="top" src={company.logo !== "0" ? company.logo : 'default-logo.png'} alt="Company Logo" style={{ height: '150px', objectFit: 'contain' }} /> */}
-                                <Card.Img src={`${getImageApi}${company.logo}`} alt={company.logo}/>
-                                <Card.Body>
-                                    <Card.Title>{company.name}</Card.Title>
-                                    <Card.Text>
-                                        <strong>Address:</strong> {company.address}
-                                        <br />
-                                        <strong>Email:</strong> {company.email}
-                                    </Card.Text>
-                                </Card.Body>
+            {companyChunks.map((chunk, index) => (
+                <Row key={index} className="mb-4">
+                    {chunk.map(company => (
+                        <Col md={6} key={company.id}>
+                            <Card style={{ padding: '20px', border: 'none' }}>
+                                <Row style={{alignItems : 'center'}}>
+                                    <Col md={3}>
+                                        <img
+                                            src={`${getImageApi}${company.logo}`} alt={company.logo}
+                                            style={{ width: '100%', borderRadius: '8px' }}
+                                        />
+                                    </Col>
+                                    <Col md={9}>
+                                        <h5>{company.name}</h5>
+                                        <p>{company.address}</p>
+                                        <p>{company.email}</p>
+                                    </Col>
+                                </Row>
                             </Card>
                         </Col>
-                    ))
-                }
-            </Row>
+                    ))}
+                </Row>
+            ))}
         </Container>
-    )
+    );
 }
+
 export default CompanyList;
